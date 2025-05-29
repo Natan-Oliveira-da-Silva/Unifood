7a383dd8b8a6ce39f2c0e15da4406840ab037e33
Diogo S <dooss2085@gmail.com>
2025-05-29 20:02:52 -0300

    Adicionada a pagina principal do cliente

--- src/src/pages/Cliente/InicioCliente/InicioCliente.jsx
+++ src/src/pages/Cliente/InicioCliente/InicioCliente.jsx
@@ -1,10 +1,42 @@
 import CabecalhoCliente from "../../../components/CabecalhoCliente/CabecalhoCliente.jsx";
 import styles from "./InicioCliente.module.css";
+import a1 from "../../../assets/a1.png"
+
 function InicioCliente() {
   return (
     <>
       <CabecalhoCliente/>
-      <h1 className={styles.titulo}>Página Principal do Cliente</h1>
+      <input type="text" className={styles.pesquisa} placeholder="Buscar restaurantes perto de você" />
+      <p className={styles.recomendacoes}>Restaurantes recomendados para você</p>
+
+      <div className={styles.Restaurantes}>
+        
+        <section className={styles.Restaurante1}>
+          <img src={a1} alt="Restaurante 1" />
+          <h2>Resaturante 1</h2>
+          <p>Nota: 3,6</p>
+        </section>
+
+         <section className={styles.Restaurante1}>
+          <img src={a1} alt="Restaurante 1" />
+          <h2>Resaturante 1</h2>
+          <p>Nota: 3,6</p>
+        </section>
+
+        <section className={styles.Restaurante1}>
+          <img src={a1} alt="Restaurante 1" />
+          <h2>Resaturante 1</h2>
+          <p>Nota: 3,6</p>
+        </section>
+
+         <section className={styles.Restaurante1}>
+          <img src={a1} alt="Restaurante 1" />
+          <h2>Resaturante 1</h2>
+          <p>Nota: 3,6</p>
+        </section>
+        
+      </div>
+      
     </>
   );
 }

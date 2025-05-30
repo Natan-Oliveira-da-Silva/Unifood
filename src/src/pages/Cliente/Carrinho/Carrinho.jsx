065dcf21100786a99ec59a2cb829426b55668a1c
Diogo S <dooss2085@gmail.com>
2025-05-29 21:38:02 -0300

    Criação do frontend da pagina Carinho do cliente

--- src/src/pages/Cliente/Carrinho/Carrinho.jsx
+++ src/src/pages/Cliente/Carrinho/Carrinho.jsx
@@ -1,12 +1,50 @@
 import CabecalhoCliente from "../../../components/CabecalhoCliente/CabecalhoCliente.jsx";
+import FooterCliente from "../../../components/FooterCliente/FooterCliente.jsx";
 import styles from "./Carrinho.module.css";
+import a1 from "../../../assets/a1.png";
+
+
 function Carrinho() {
   return (
     <>
-      <CabecalhoCliente/>
-      <h1 className={styles.titulo}>Carrinho</h1>
+      <div className={styles.CarrinhoPage}>
+        <CabecalhoCliente/>
+      
+        <main className={styles.carrinhoMainContent}>
+
+          <h1 className={styles.titulo}>Carrinho</h1>
+
+          <div className={styles.Carrinho}>
+
+            <section className={styles.Logo}>
+              <img src={a1} alt="Logo do restaurante" className={styles.foto} />
+
+            </section>
+
+            <section className={styles.Logo}>
+              <p>Item:</p>
+              <p>Preço:</p>
+              <p>Restaurante:</p>
+
+            </section>
+
+            <section className={styles.Logo}>
+              <p>quantidade:</p>
+
+            </section>
+
+          </div>  
+
+        </main>
+
+        <FooterCliente/>
+
+      </div>
+    
     </>
   );
 }
 
 export default Carrinho;
+
+
